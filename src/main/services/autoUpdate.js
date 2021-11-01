ceddaf444a39cf5e5a7b0c01176348cd56cf555a
RUNNERUP <raj2011@163.com>
2021-11-01 16:07:20 +0800

    feat: mac auto update

--- src/main/services/autoUpdate.js
+++ src/main/services/autoUpdate.js
@@ -2,8 +2,8 @@ import { ipcMain } from 'electron';
 // 注意这个autoUpdater不是electron中的autoUpdater
 import { autoUpdater } from 'electron-updater';
 
-autoUpdater.logger = require("electron-log")
-autoUpdater.logger.transports.file.level = "info"
+autoUpdater.logger = require('electron-log');
+autoUpdater.logger.transports.file.level = 'info';
 // 更新服务器地址，比如"http://**.**.**.**:3002/download/"
 
 // 检测更新，在你想要检查更新的时候执行，renderer事件触发后的操作自行编写
@@ -25,7 +25,7 @@ function updateHandle(mainWindow) {
     private: false,
     repo: 'MegSpot',
     owner: 'MegEngine',
-    releaseType: 'release',
+    releaseType: 'release'
   });
 
   autoUpdater.on('error', function(info) {
@@ -87,12 +87,12 @@ function updateHandle(mainWindow) {
       });
   });
   // 后端主动检查版本更新
-  // if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     autoUpdater.checkForUpdates();
     setInterval(() => {
       autoUpdater.checkForUpdates();
     }, 60 * 60 * 1000);
-  // }
+  }
 }
 // }
 
